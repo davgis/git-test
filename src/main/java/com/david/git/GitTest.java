cb36bdd6689f7b30010f8087b222972229a020ed
David <davgis@163.com>
2021-11-11 11:06:56 +0800

    hot-fix commit2

--- src/main/java/com/david/git/GitTest.java
+++ src/main/java/com/david/git/GitTest.java
@@ -6,5 +6,7 @@ public class GitTest {
         System.out.println("hello Maven!!!!!!");
         System.out.println("hello IDEA!!!!!!");
         System.out.println("hello Git2!!!!!!");
+        System.out.println("hello Git2!!!!!!");
+        System.out.println("hot-fix modified code~~~");
     }
 }
