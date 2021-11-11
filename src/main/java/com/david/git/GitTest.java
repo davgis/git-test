8b1a1bc57472c6e3b9b024d653358d45f4ee2680
David <davgis@163.com>
2021-11-11 10:15:55 +0800

    second commit

--- src/main/java/com/david/git/GitTest.java
+++ src/main/java/com/david/git/GitTest.java
@@ -2,6 +2,7 @@ package com.david.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!");
+        System.out.println("hello Git!");
+        System.out.println("hello Maven!!!!!!");
     }
 }
